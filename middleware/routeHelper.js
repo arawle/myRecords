a8b66beb2e9c6227635ec199e2c6763e80211c30
Austen <rawleag@gmail.com>
2015-09-21 09:46:45 -0700

    route helpers added

---
+++ middleware/routeHelper.js
@@ -0,0 +1,31 @@
+var db = require('../models');
+
+var routeHelpers = {
+  ensureLoggedIn: function(req, res, next) {
+    if (req.session.id !== null && req.session.id !== undefined) {
+      return next();
+    } else {
+      res.redirect('/login');
+    }
+  },
+
+  ensureCorrectUser: function(req, res, next) {
+    db.Record.findById(req.params.id, function(err, record) {
+      if (res.locals.user != req.session.id) {
+        res.redirect('/records');
+      } else {
+        return next();
+      }
+    });
+  },
+
+  preventLoginSignup: function(req, res, next) {
+    if (req.session.id !== null && req.session.id !== undefined) {
+      res.redirect('/records');
+    } else {
+      return next();
+    }
+  },
+};
+
+module.exports = routeHelpers;
