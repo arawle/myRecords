6f484bf380a4c1bc6b93f749e5eb1660e7a1ed46
Austen <rawleag@gmail.com>
2015-09-21 09:38:39 -0700

    album art model done

---
+++ models/albumArt.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var artSchema = new mongoose.Schema({
+  colorBW: Boolean,
+  artTheme: String,
+  artists: Array,
+  size: String,
+  dieCut: Boolean,
+  shape: String,
+  objectsInside: String,
+  mistakes: String,
+  colorTheme: String,
+  coverCondition: String,
+});
+
+var Art = mongoose.model('Art', artSchema);
+
+module.exports = Art;
