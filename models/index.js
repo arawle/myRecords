071f24a4cb83686f79406f7a18c503d34c5e6596
Austen <rawleag@gmail.com>
2015-09-21 09:16:58 -0700

    added index model and linked up the other models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+mongoose.connect();
+
+//require other models
+module.exports.User = require('./user');
+module.exports.Art = require('./albumArt');
+module.exports.Artist = require('./artist');
+module.exports.Music = require('./music');
+module.exports.Physical = require('./physical');
+module.exports.Record = require('./record');
