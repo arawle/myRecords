600e454b62e67e43880f8f15c4b2403ef3a59842
Austen <rawleag@gmail.com>
2015-09-25 15:18:37 -0700

    searches genre, tracks, and lead artist, all array categories on record model changed to string

--- controller/search.js
+++ controller/search.js
@@ -4,23 +4,13 @@ var mongoose = require('mongoose');
 app.get('/search', function (req, res) {
   var request = require('request');
   var yourSearch = req.query.search.toString();
-  db.Record.find({"genre": yourSearch}, function(err, found) {
+  db.Record.find(({"genre": yourSearch}, {"leadArtists": yourSearch}, {"tracks": yourSearch}), function(err, found) {
     if (err) {
       console.log(err);
     } else {
       databaseSearch = found;
       console.log(databaseSearch)
+      res.render('users/search', {data: found})
     }
   });
-
-
-  // request.get(website, function (error, response, body) {
-  //   if (error) {
-  //     console.log("Error!  Request failed - " + error);
-  //   } else if (!error && response.statusCode === 200) {
-  //     console.log(body);
-  //     var parseIt = JSON.parse(body);
-  //      res.render('/search', {data: parseIt.items})
-  //   }
-  // });
 })
