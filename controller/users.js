baaf94bffbbb8fc51c48f9272aa571046ab767f2
Austen <rawleag@gmail.com>
2015-09-24 17:14:21 -0700

    admin page w/ password created to add admins'

--- controller/users.js
+++ controller/users.js
@@ -12,12 +12,22 @@ app.get('/logout', function(req, res) {
 
 app.get('/users/:id', function(req, res) {
   db.User.findById(req.params.id).populate('records recordOfWeek recordOfMonth').exec(function(err, data) {
-    console.log(data)
     if (err) {
       console.log(err);
       res.render('404');
     } else {
       res.render('users/userProfile', {user: data});
+    }
+  });
+});
+
+app.get('/admin', function(req, res) {
+  db.User.findById(req.params.id, function(err, data) {
+    if (err) {
+      console.log(err);
+      res.render('404');
+    } else {
+      res.render('users/admin', {user: data});
     }
   });
 });
@@ -32,6 +42,20 @@ app.put('/users', function(req, res) {
     }
   });
 });
+
+app.put('/admins', function(req, res) {
+  db.User.findByIdAndUpdate(req.session.id, req.body, function(err, user) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (req.body.adminPass === 'c0ngr@t5@dm!n') {
+        user.admin = true;
+        user.save;
+      }
+        res.redirect('/users/' + req.session.id)
+    }
+  })
+})
 
 app.post('/signup', function(req, res) {
   var newUser = req.body.user;
