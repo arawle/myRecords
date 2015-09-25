2a43428b4653359e0fd4edc713b825c776d05bd1
Austen <rawleag@gmail.com>
2015-09-24 17:16:28 -0700

    only admins can change the featured records

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  // $details = $('.details');
-  console.log('hi');
-
   $('.details').on('click', function(e) {
     var thisId = $(this).data('id');
     var url = 'http://localhost:3000/records/' + thisId;
@@ -35,5 +32,4 @@ $(document).ready(function() {
       alert('Record of the Week Set Successfully!')
     })
   });
-
 });
