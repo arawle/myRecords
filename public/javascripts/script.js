900f9c4a42a21610fb16cfa050e195649ed8f3e6
Austen <rawleag@gmail.com>
2015-09-28 09:25:25 -0700

    added a delete button on record edit form

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
+  //goes to the more info- edit page for records
   $('.details').on('click', function(e) {
     var thisId = $(this).data('id');
     var url = 'http://localhost:3000/records/' + thisId;
     window.location.href = url;
   });
-
+  //set record of month button
   $('.setRecordMonth').on('click', function(e) {
     var record = $(this).data('record');
     var dataUrl = $(this).data('url');
@@ -18,7 +19,7 @@ $(document).ready(function() {
       alert('Record of the Month Set Successfully!');
     });
   });
-
+  //set record of week button
   $('.setRecordWeek').on('click', function(e) {
     var record = $(this).data('record');
     var dataUrl = $(this).data('url');
@@ -32,9 +33,13 @@ $(document).ready(function() {
       alert('Record of the Week Set Successfully!');
     });
   });
-
+  //removes the search by artist, genre, etc. field for easier searching
   $('#search').on('click', function(e) {
     ($(this).attr('value', ''));
   });
+
+  $('.delete').on('click', function(e) {
+    console.log('delete button')
+  })
 
 });
