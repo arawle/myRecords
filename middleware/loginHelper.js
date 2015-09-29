7c0af006487e66765433552e47ce4a32e167eac1
Austen <rawleag@gmail.com>
2015-09-29 07:44:10 -0700

    heroku edits

--- middleware/loginHelper.js
+++ middleware/loginHelper.js
@@ -15,7 +15,7 @@ var loginHelpers = function(req, res, next) {
     res.locals.user = req.session.id;
   }
 
-  db.User.findById('56056d08c1f6e05654af18da').populate('records recordOfWeek recordOfMonth').exec(function(err, superUser) {
+  db.User.findById('5609d3f34035e40300d1c33d').populate('records recordOfWeek recordOfMonth').exec(function(err, superUser) {
     if (err) {
       console.log(err);
     } else {
